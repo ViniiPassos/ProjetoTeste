c9853e8c85b373f6f2321a285fde5840ef306209
Vinicius Passos <vinicius.pmp19@gmail.com>
2023-11-20 12:35:21 -0300

    Concluido.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,53 @@
+
 namespace ProjetoTeste
 {
     public class Tests
     {
+        public int a, b, soma, multi, resultadoSoma, resultadoMulti;
         [SetUp]
         public void Setup()
         {
+            a = 5; b = 7;
+
+            soma = a + b;
+            resultadoSoma = 12;
+
+            Assert.That(soma, Is.EqualTo(resultadoSoma));
+            Console.WriteLine("O resultado da soma é: " + resultadoSoma);
+
         }
 
         [Test]
-        public void Test1()
+        public void Multi()
         {
-            Assert.Pass();
+            a = 5; b = 7;
+
+            multi = a * b;
+            resultadoMulti = 35;
+
+            Assert.That(multi, Is.EqualTo(resultadoMulti));
+            Console.WriteLine("O resultado da multiplicação é: " + resultadoMulti);
+        }
+
+        [TearDown]
+        public void Resultado()
+        {
+
+            if(soma != resultadoSoma)
+            {
+                Console.WriteLine("O valor da soma deve ser: " + soma);
+                Console.WriteLine("O valor digitado foi: "+ resultadoSoma); Assert.Fail();
+            }
+
+            if (multi != resultadoMulti)
+            {
+                Console.WriteLine("O valor da multiplicação deve ser: " + multi);
+                Console.WriteLine("O valor digitado foi: " + resultadoMulti); Assert.Fail();
+            }
+            else
+            {
+                Console.WriteLine("Muito bem! Você respondeu corretamente!");
+            }
         }
     }
 }
